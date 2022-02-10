4382ba0c1370500592b35cc4b29587904201b6eb
Barta Balázs <barta.balazs00@gmail.com>
2022-02-10 12:03:08 +0100

    view-ok elkészítése

---
+++ src/main/java/com/example/muzeumfrontendjavafx/modositFestmenyController.java
@@ -0,0 +1,22 @@
+package com.example.muzeumfrontendjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class modositFestmenyController {
+
+    @FXML
+    private TextField inputCim;
+    @FXML
+    private CheckBox inputKiallitva;
+    @FXML
+    private Spinner inputEv;
+
+    @FXML
+    public void onModositButtonClick(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/com/example/muzeumfrontendjavafx/modositSzoborController.java
@@ -0,0 +1,20 @@
+package com.example.muzeumfrontendjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class modositSzoborController {
+    @FXML
+    private TextField inputSzemely;
+    @FXML
+    private TextField inputMagassag;
+    @FXML
+    private Spinner inputAr;
+
+    @FXML
+    public void onModositButtonClick(ActionEvent actionEvent) {
+
+    }
+}
