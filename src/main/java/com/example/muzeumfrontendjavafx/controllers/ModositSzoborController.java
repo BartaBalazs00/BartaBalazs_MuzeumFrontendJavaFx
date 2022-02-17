fd72656b209f9794afd7205c0fad384a691b9437
BartaBalazs00 <77434768+BartaBalazs00@users.noreply.github.com>
2022-02-17 11:22:38 +0100

    szobor módosítás

--- src/main/java/com/example/muzeumfrontendjavafx/controllers/ModositSzoborController.java
+++ src/main/java/com/example/muzeumfrontendjavafx/controllers/ModositSzoborController.java
@@ -1,11 +1,12 @@
 package com.example.muzeumfrontendjavafx.controllers;
 
-import com.example.muzeumfrontendjavafx.Controller;
-import com.example.muzeumfrontendjavafx.Statue;
+import com.example.muzeumfrontendjavafx.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class ModositSzoborController extends Controller {
     @FXML
@@ -17,7 +18,45 @@ public class ModositSzoborController extends Controller {
     private Statue modositando;
     @FXML
     public void onModositButtonClick(ActionEvent actionEvent) {
+        String person = inputPerson.getText().trim();
+        int height, price;
+        if (person.isEmpty()){
+            alert("A személy megadása kötelező");
+            return;
+        }
+        try {
+            height = inputHeight.getValue();
+        } catch (NullPointerException ex){
+            alert("A magasság megadása kötelező");
+            return;
+        } catch (Exception ex){
+            alert("A magasság csak 1 és 999999999 közötti szám lehet");
+            return;
+        }
+        try {
+            price = inputPrice.getValue();
+        } catch (NullPointerException ex){
+            alert("Az ár megadása kötelező");
+            return;
+        } catch (Exception ex){
+            alert("Az ár csak 1 és 999999999 közötti szám lehet");
+            return;
+        }
+        modositando.setPerson(person);
+        modositando.setHeight(height);
+        modositando.setPrice(price);
 
+        try {
+            Statue modositott = StatueApi.updateStatue(modositando);
+            if (modositott != null){
+                alertWait("Sikeres módosítás");
+                this.stage.close();
+            } else {
+                alert("Sikertelen módosítás");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public Statue getModositando() {
         return modositando;
